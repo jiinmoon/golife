53c8f25f59ed0e16835cd1ed4b5a3418aa6fd476
Ji-In Moon <jiinmoon@tutanota.com>
2020-07-19 21:15:43 -0700

    added .gitignore file.

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 """ main.py """
+
+
+class GameBoard:
+    def __init__(self):
+        self.WIDTH = 0
+        self.HEIGHT = 0
+        self.BOARD_STATE = []
+
+    def dead_state(self, width, height):
+        """ returns non-trival dead state of size width * height """
+        self.BOARD_STATE = [[0] * width for _ in range(height)]
+        return self.BOARD_STATE
+
